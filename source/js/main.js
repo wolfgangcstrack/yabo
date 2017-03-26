31a60c6d1d6254180a1294fc9d81526ede7867aa
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2017-03-25 17:34:47 -0700

    Update fadeIn to work with jQuery instead of CSS (which messed with side-nav)

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+  // Trigger mobile side-nav when .button-collapse is clicked
   $(".button-collapse").sideNav();
+
+  var one_second = 1000;
+  var transition = 1100;
+
+  for (var i = 0; i <= 10; i++) {
+    $(".fade-in-" + i).delay(i * one_second).fadeIn(transition);
+  }
 });
